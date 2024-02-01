c695c0a9a17d5bbb7fa13bdf8fa91866f0a147af
cyberkrane <cyberkrane@gmail.com>
2024-02-01 18:14:26 -0300

    arquitectura terminada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './index/home/home.component';
+import { SecondPageComponent } from './index/second-page/second-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'home', component:HomeComponent},
+  {path:'second-page', component:SecondPageComponent},
+  {path:'**', redirectTo:''},
+  {path:'**', pathMatch:'full', redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './index/home/home.component';
+import { SecondPageComponent } from './index/second-page/second-page.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SecondPageComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
